1fe1928fce0d34b8070180068b39840a08ba2945
Samuel Malec <malec293@gmail.com>
2026-09-20 17:03:05 +0200

    analysis: improve the analysis scheduling logic

--- src/analysis/pass_manager.hpp
+++ src/analysis/pass_manager.hpp
@@ -7,12 +7,22 @@
 
 namespace dungeon::analysis {
     struct pass_manager {
-        std::vector<std::unique_ptr<pass> > passes;
+        std::vector<std::unique_ptr<pass> > owned;
+        std::vector<pass *> schedule;
 
-        void add(std::unique_ptr<pass> pass) { passes.push_back(std::move(pass)); }
+        pass *add(std::unique_ptr<pass> pass) {
+            owned.push_back(std::move(pass));
+            auto raw = owned.back().get();
+            schedule.push_back(raw);
+            return raw;
+        }
+
+        void repeat(pass *p) {
+            schedule.push_back(p);
+        }
 
         void run(ir::function &fn) {
-            for (auto &pass: passes)
+            for (auto pass: schedule)
                 pass->run(fn);
         }
     };
--- src/analysis/pipeline.hpp
+++ src/analysis/pipeline.hpp
@@ -4,16 +4,19 @@
 #include "mem2reg.hpp"
 #include "pass_manager.hpp"
 #include "sccp.hpp"
+#include "simplify_cfg.hpp"
 
 namespace dungeon {
+    // TODO: add a mechanism to check if preconditions of individual passes are satisfied
     inline analysis::pass_manager get_default_pipeline() {
         analysis::pass_manager pm;
-        // TODO: add a mechanism to check if preconditions of individual passes are satisfied
+        pass *cfg = pm.add(std::make_unique<simplify_cfg>());
         pm.add(std::make_unique<mem2reg>());
         pm.add(std::make_unique<sccp>());
         pm.add(std::make_unique<dce>());
+        pm.repeat(cfg);
         pm.add(std::make_unique<reg2mem>());
-        
+
         return pm;
     }
 }
